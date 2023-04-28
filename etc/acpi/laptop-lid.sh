b39cd0599e6d75661cc23aef58fa30dad6a24fb5
Behemod <103079382+Behemod@users.noreply.github.com>
2023-04-29 09:51:27 +1000

    perfection

--- etc/acpi/laptop-lid.sh
+++ etc/acpi/laptop-lid.sh
@@ -2,8 +2,11 @@
 
 # Events on lid switching states(/proc/acpi/button/lid/LID/state), called by /etc/acpi/events/laptop-lid
 
-sh -c "echo mem > /sys/power/state"
-systemctl restart systemd-networkd && systemctl restart openvpn
+cat /proc/acpi/button/lid/LID/state | grep closed -q
+if [[ $? -eq 0 ]]; then
+    sh -c "echo mem > /sys/power/state"
+    systemctl restart systemd-networkd && systemctl restart openvpn
+fi
 
 # Dont forget make it executable
 # To make changes take effect: sudo /etc/init.d/acpid restart
